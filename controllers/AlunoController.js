eca07cdbbf9a60f61e6838751887b3769df07354
cauany21 <cauanypinto.bg022@academico.ifsul.edu.br>
2025-08-21 11:01:37 -0300

    aula!

---
+++ controllers/AlunoController.js
@@ -0,0 +1,25 @@
+import Aluno from '../models/aluno.js'
+
+export default class AlunoController{
+
+    constructor(caminhoBase='aluno/'){
+         this.caminhoBase = caminhoBase
+    
+
+        this.openAdd = async(req, res)=>{
+            res.render(caminhoBase + add)
+        }
+        this.add = async(req, res)=>{
+            //criar aluno
+            await Alunocreat({
+                nome: req.body.nome,
+                matricula: req.body.matricula
+            });
+            res.redirect('/'+ caminhoBase + 'add');
+        }
+        this.list =  async(req,res)=>{
+            const resultado = await Aluno.find({})
+            res.render(caminhoBase + 'lst',{Alunos:resultado})
+        }
+}
+}
